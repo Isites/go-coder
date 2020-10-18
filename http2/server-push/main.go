b6a4237b74ac80116667df1d701568ce475d2038
wxu <23shuaixw@gmail.com>
2020-10-18 18:55:28 +0800

    修改http2 服务起推送例子

--- http2/server-push/main.go
+++ http2/server-push/main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-const indexHTML = `<html>
-<head>
-	<title>新世界杂货铺</title>
-	<link rel="stylesheet" href="/static/style.css"">
-</head>
-<body>
-	<div>Hello 新世界杂货铺</div>
-	<div id="content"></div>
-	<script src="/static/app.js"></script>
-</body>
-</html>
-`
-
-var httpAddr = flag.String("http", ":8080", "Listen address")
-
 func main() {
-	flag.Parse()
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			http.NotFound(w, r)
@@ -32,8 +14,7 @@ func main() {
 		}
 		pusher, ok := w.(http.Pusher)
 		if ok {
-			// Push is supported. Try pushing rather than
-			// waiting for the browser request these static assets.
+			// 主动推送服务资源
 			if err := pusher.Push("/static/app.js", nil); err != nil {
 				log.Printf("Failed to push: %v", err)
 			}
@@ -41,7 +22,19 @@ func main() {
 				log.Printf("Failed to push: %v", err)
 			}
 		}
-		fmt.Fprintf(w, indexHTML)
+		fmt.Fprintf(w, `<html>
+		<head>
+			<title>新世界杂货铺</title>
+			<link rel="stylesheet" href="/static/style.css"">
+		</head>
+		<body>
+			<div>Hello 新世界杂货铺</div>
+			<div id="content"></div>
+			<script src="/static/app.js"></script>
+		</body>
+		</html>`)
 	})
-	log.Fatal(http.ListenAndServeTLS(*httpAddr, "ca2.crt", "ca2.key", nil))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// http.ListenAndServe(":8080", nil)
+	http.ListenAndServeTLS(":8080", "ca.crt", "ca.key", nil)
 }
